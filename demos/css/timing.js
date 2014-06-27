db72dc3eab7b0f1b1c5c80573aa8e3e24ddf628b
Christopher Van <cvan@mozilla.com>
2014-06-26 23:37:08 -0700

    add CSS test runner

--- demos/css/timing.js
+++ demos/css/timing.js
@@ -5,10 +5,22 @@ window.onload = setTimeout(function () {
   var connection = timing.connectEnd - timing.connectStart;
   var requestTime = timing.responseEnd - timing.requestStart;
   var fetchTime = timing.responseEnd - timing.fetchStart;
-  console.log(
-    'User-perceived page loading time: ' + pageLoadTime + ' ms \n' +
-    'Connection time: ' + connection + ' ms \n' +
-    'DNS Time: ' + dns + ' ms \n' +
-    'Request Time: ' + requestTime + ' ms \n' +
-    'Fetch Time: ' + fetchTime + ' ms \n');
+  // console.log(
+  //   'User-perceived page loading time: ' + pageLoadTime + ' ms \n' +
+  //   'Connection time: ' + connection + ' ms \n' +
+  //   'DNS Time: ' + dns + ' ms \n' +
+  //   'Request Time: ' + requestTime + ' ms \n' +
+  //   'Fetch Time: ' + fetchTime + ' ms \n');
 });
+
+// function cssloaded(time) {
+//   console.log('CSS loading time: ' + time.toFixed(4) + ' ms');
+//   clearInterval(ti);
+//   if (window.top !== window) {
+//     // If `iframe`d, `postMessage` the result to the parent window.
+//     window.top.postMessage({
+//       type: 'cssloaded',
+//       data: ti
+//     }, '*');
+//   }
+// }
